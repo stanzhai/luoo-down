bba1b1796149d1a24a09bd7ed3d890f50b103634
stanzhai <jasondan325@163.com>
2014-05-23 23:24:19 +0800

    improve save and open mp3 logic,add fm info

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var http = require('http')
+  , fs = require('fs')
+  , path = require('path')
   , request = require('request')
   , progress = require('request-progress')
   , ProgressBar = require('progress')
-  , fs = require('fs')
+  , open = require('open')
   , List = require('./lib/term-list-enhanced');
 
 var bar = new ProgressBar('正在下载：:title [:bar] :percent :etas', {
@@ -16,12 +18,28 @@ var menu = new List({
   labelKey: 'title'
 });
 
-request('http://www.luoo.net/', function (err, res, html) {
-  var key = 'var volPlaylist = ';
-  var start = html.indexOf(key);
-  var end = html.indexOf('}];', start);
-  var playList = JSON.parse(html.substring(start + key.length, end + 2));
+// make download dir if not exists
+var downloadDir = './downloads'
+if (!fs.existsSync(downloadDir)) {
+  fs.mkdirSync(downloadDir);
+}
+var currFm = '';
 
+request('http://www.luoo.net/', function (err, res, html) {
+  var playList = JSON.parse(findContent(html, 'var volPlaylist = ', '}];', 2));
+  var fmTitle = findContent(html, '<h1 class="fm-title">', '</h1>', 0);
+  var fmIntro = findContent(html, '<p class="fm-intro">', '</p>', 0);
+  var fmCover = 'http://img' + findContent(html, 'http://img', '"', 0);
+  var fmPath = path.join(downloadDir, fmTitle);
+  var introPath = path.join(downloadDir, fmTitle, fmTitle + '.txt');
+  var coverPath = path.join(downloadDir, fmTitle, fmTitle + '.jpg')
+  if (!fs.existsSync(fmPath)) {
+    fs.mkdirSync(fmPath);
+    fs.writeFile(introPath, fmIntro.replace(/<br>/g, '\r\n'));
+    request(fmCover).pipe(fs.createWriteStream(coverPath));
+  }
+
+  currFm = fmTitle;
   menu.adds(playList);
   menu.start();
 });
@@ -29,30 +47,43 @@ request('http://www.luoo.net/', function (err, res, html) {
 menu.on('keypress', function(key, index) {
   if (key.name === 'return') {
     var item = menu.item(index);
-    if (bar.curr != 0) {
-      return;   // is downloading
-    } 
+    //if (bar.curr != 0) {
+    //  return;   // is downloading
+    //} 
     downloadMP3(item);
   } else if (key.name === 'q') {
     return menu.exit();
   }
 });
 
+function findContent(html, key, endTag, offset) {
+  var start = html.indexOf(key);
+  var end = html.indexOf(endTag, start);
+  return html.substring(start + key.length, end + offset);
+}
+
 // 下载歌曲
 function downloadMP3(mp3Info) {
-  request(mp3Info.poster).pipe(fs.createWriteStream(mp3Info.title + '.jpg'));
+  var coverFile = path.join(downloadDir, currFm, mp3Info.title + '.jpg');
+  request(mp3Info.poster).pipe(fs.createWriteStream(coverFile));
 
-  var lastReceived = 0;
-  progress(request(mp3Info.mp3))
-  .on('progress', function (state) {
-    bar.total = state.total;
-    bar.tick(state.received - lastReceived, {title: mp3Info.title});
-    lastReceived = state.received;
-  })
-  .pipe(fs.createWriteStream(mp3Info.title + '.mp3'))
-  .on('close', function (err) {
-    // 下载结束后，重置下载条状态
-    bar.tick(bar.total - bar.curr);
-    bar.curr = 0;
-  });
+  var mp3File = path.join(downloadDir, currFm, mp3Info.title + '.mp3');
+  if (!fs.existsSync(mp3File)) {
+    var lastReceived = 0;
+    progress(request(mp3Info.mp3))
+    .on('progress', function (state) {
+      bar.total = state.total;
+      bar.tick(state.received - lastReceived, {title: mp3Info.title});
+      lastReceived = state.received;
+    })
+    .pipe(fs.createWriteStream(mp3File))
+    .on('close', function (err) {
+      // 下载结束后，重置下载条状态
+      bar.tick(bar.total - bar.curr);
+      bar.curr = 0;
+    });
+  } else {
+    open(mp3File);
+  }
+
 }
